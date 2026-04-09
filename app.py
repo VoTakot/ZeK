5bc7d5c6a19d51a0dbd76b9102647ecfb5e77de0
VoTakot <matveisergeeiv2012@gmail.com>
2026-04-09 18:44:25 +0300

    initial commit (3)

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'VmqESHIfRwKUPoBVjbtO'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
 @app.route('/')
@@ -11,4 +15,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', 8080)
+    app.run('127.0.0.1',    8080)
